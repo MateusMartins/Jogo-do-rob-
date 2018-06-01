987a8051ecca4a3b9d878c0d65c48c9245ac24c8
Mateus Ferreira <mateusmferreira@live.com>
2018-06-01 15:25:58 -0300

    Jogo finalizado

--- index.py
+++ index.py
@@ -2,15 +2,15 @@
 from robo import Robot
 from robo import Reward
 from robo import Validacao
-import random
+from random import randint
 
 #Criação dos robôs com as posições x e y na matriz
-caminhoneiro = Robot(random.randint(0,10),random.randint(0,10))
+caminhoneiro = Robot(randint(0,10),randint(0,10))
 
 #Estanciação das recompensas presentes na matriz
-comida = Reward(random.randint(0,10),random.randint(0,10),'Comida')
-gasolina = Reward(random.randint(0,10),random.randint(0,10),'Gasolina')
-arma = Reward(random.randint(0,10),random.randint(0,10),'Arma')
+comida = Reward(randint(0,10),randint(0,10),'Comida')
+gasolina = Reward(randint(0,10),randint(0,10),'Gasolina')
+arma = Reward(randint(0,10),randint(0,10),'Arma')
 
 #Criação da lista de recompensas
 rewards = [comida, gasolina, arma]
@@ -19,7 +19,7 @@ rewards = [comida, gasolina, arma]
 Validador = Validacao()
 
 print('Posição inicial do caminhoneiro na matriz: %s' % (caminhoneiro))
-for i in range(10):
+for i in range(20):
     movimento = input("Digite up, down, left ou right para o movimento: ")
     if movimento == 'up':
         caminhoneiro.move_up()
@@ -33,7 +33,7 @@ for i in range(10):
         print('Movimento inválido')
         continue #se chegar no contiue o sistema ignora o loop atual e continua o ciclo de repetição ignorando o código abaixo
     
-    print('Número de tentativas %d' % (i))
+    print('Número de tentativas %d' % (19 - i))
     print('Posição atual do caminhoneiro na matriz: %s' % (caminhoneiro))
     #Validação da recompensa
     Validador.check_reward(caminhoneiro,rewards)
